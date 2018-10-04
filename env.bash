1d7d463cf682890f2ef9c9b19c5564d97aa9628a
Hank <hank.chang@technexion.com>
2018-10-04 10:39:07 +0800

    PICO-8M: Add support for Android Oreo

--- env.bash
+++ env.bash
@@ -1,76 +1,75 @@
 #############################
 # Author
-# Wig Cheng@TW
+# Technexion
 #############################
 
 TOP="${PWD}"
-PATH_KERNEL="${PWD}/kernel_imx"
-PATH_UBOOT="${PWD}/bootable/bootloader/uboot-imx"
+PATH_KERNEL="${PWD}/vendor/nxp-opensource/kernel_imx"
+PATH_UBOOT="${PWD}/vendor/nxp-opensource/uboot-imx"
 
 export PATH="${PATH_UBOOT}/tools:${PATH}"
-export ARCH=arm
+#export ARCH=arm
 #export CROSS_COMPILE="${PWD}/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
-export CROSS_COMPILE="${PWD}/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-"
+#export CROSS_COMPILE="${PWD}/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-"
+#export CROSS_COMPILE=/opt/gcc-5.1-2015.08-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
+CLASSPATH=".:$JAVA_HOME/lib:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar"
+#PATH="$JAVA_HOME/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games"
+PATH="$JAVA_HOME/bin:${PATH}"
+export DISPLAY=:0
+#export PATH=$PATH:/opt/gcc-5.1-2015.08-x86_64_arm-linux-gnueabihf/bin
+#export CROSS_COMPILE=/opt/gcc-5.1-2015.08-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+#export PATH=$PATH:"${TOP}/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin"
+#export CROSS_COMPILE="${TOP}/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+#sudo apt-get install gcc-aarch64-linux-gnu
+#sudo apt-get install gcc-arm-linux-gnueabi
+
+export ARCH=arm64
+export CROSS_COMPILE=/usr/bin/aarch64-linux-gnu-
+export CROSS32CC=arm-linux-gnueabi-gcc
+
+export MY_ANDROID=$TOP
+export LC_ALL=C
+source build/envsetup.sh
 
 # TARGET support: wandboard,edm1cf,picosom,edm1cf_6sx
 IMX_PATH="./mnt"
+SYS_PATH="./tmp"
 MODULE=$(basename $BASH_SOURCE)
 CPU_TYPE=$(echo $MODULE | awk -F. '{print $3}')
 CPU_MODULE=$(echo $MODULE | awk -F. '{print $4}')
 BASEBOARD=$(echo $MODULE | awk -F. '{print $5}')
 OUTPUT_DISPLAY=$(echo $MODULE | awk -F. '{print $6}')
 KERNEL_CFLAGS='KCFLAGS=-mno-android'
-KERNEL_CONFIG='tn_imx_android_defconfig'
 
-if [[ "$CPU_TYPE" == "imx6" ]]; then
-    if [[ "$CPU_MODULE" == "edm1cf-sd" ]]; then
-        if [[ "$BASEBOARD" == "wandboard" ]]; then
-            UBOOT_CONFIG='wandboard_defconfig'
-            KERNEL_IMAGE='zImage'
-            KERNEL_CONFIG='wandboard_android_defconfig'
-            DTB_TARGET='imx6q-wandboard-revc1.dtb imx6dl-wandboard-revc1.dtb imx6q-wandboard-revb1.dtb imx6dl-wandboard-revb1.dtb'
-            TARGET_DEVICE=wandboard
+PATH_TOOLS="${TOP}/device/fsl/common/tools"
+
+if [[ "$CPU_TYPE" == "imx8" ]]; then
+    if [[ "$CPU_MODULE" == "pico-8m" ]]; then
+        if [[ "$BASEBOARD" == "pi" ]]; then
+            UBOOT_CONFIG='pico_8m_android_defconfig'
+            KERNEL_IMAGE='Image'
+            KERNEL_CONFIG='android_defconfig'
+            DTB_TARGET='pico_8m.dtb'
+            TARGET_DEVICE=pico_8m
+            TARGET_DEVICE_NAME=imxpico_8m
+        elif [[ "$BASEBOARD" == "wanboard" ]]; then
+            UBOOT_CONFIG='mx8mq_evk_android_defconfig'
+            KERNEL_IMAGE='Image'
+            KERNEL_CONFIG='android_defconfig'
+            DTB_TARGET='fsl-imx8mq-evk.dtb'
+            TARGET_DEVICE=evk_8mq
+            TARGET_DEVICE_NAME=imx8mq
         fi
-
-    elif [[ "$CPU_MODULE" == "edm1cf-nand" ]]; then
-        if [[ "$BASEBOARD" == "fairy" ]]; then
-            UBOOT_CONFIG='edm-cf-imx6_defconfig'
-            KERNEL_IMAGE='uImage LOADADDR=0x10008000'
-            DTB_TARGET='imx6q-edm1-cf_fairy.dtb imx6dl-edm1-cf_fairy.dtb imx6q-edm1-cf-pmic_fairy.dtb imx6dl-edm1-cf-pmic_fairy.dtb imx6qp-edm1-cf-pmic_fairy.dtb'
-            TARGET_DEVICE=edm1cf_6dq
-        elif [[ "$BASEBOARD" == "tc0700" ]]; then
-            UBOOT_CONFIG='edm-cf-imx6-no-console_defconfig'
-            KERNEL_IMAGE='uImage LOADADDR=0x10008000'
-            DTB_TARGET='imx6q-edm1-cf_tc0700.dtb imx6dl-edm1-cf_tc0700.dtb imx6q-edm1-cf-pmic_tc0700.dtb imx6dl-edm1-cf-pmic_tc0700.dtb imx6qp-edm1-cf-pmic_tc0700.dtb'
-            TARGET_DEVICE=edm1cf_6dq
-        fi
-
-    elif [[ "$CPU_MODULE" == "edm1cf-nand-pmic" ]]; then
-        if [[ "$BASEBOARD" == "fairy" ]]; then
-            UBOOT_CONFIG='edm-cf-imx6_defconfig'
-        elif [[ "$BASEBOARD" == "tc0700" ]]; then
-            UBOOT_CONFIG='edm-cf-imx6-no-console_defconfig'
-        fi
-        KERNEL_IMAGE='uImage LOADADDR=0x10008000'
-        DTB_TARGET='imx6q-edm1-cf_fairy.dtb imx6dl-edm1-cf_fairy.dtb imx6q-edm1-cf-pmic_fairy.dtb imx6dl-edm1-cf-pmic_fairy.dtb imx6qp-edm1-cf-pmic_fairy.dtb'
-        TARGET_DEVICE=edm1cf_pmic_6dq
-
-
-    elif [[ "$CPU_MODULE" == "pico-sd" ]]; then
-            UBOOT_CONFIG='pico-imx6_defconfig'
-            KERNEL_IMAGE='uImage LOADADDR=0x10008000'
-            DTB_TARGET='imx6q-pico_dwarf.dtb imx6dl-pico_dwarf.dtb imx6q-pico_hobbit.dtb imx6dl-pico_hobbit.dtb imx6q-pico_nymph.dtb imx6dl-pico_nymph.dtb'
-            TARGET_DEVICE=pico_6dq
-
-    elif [[ "$CPU_MODULE" == "tek3" || "$CPU_MODULE" == "tep" ]]; then
-            UBOOT_CONFIG='tek-imx6_defconfig'
-            KERNEL_IMAGE='uImage LOADADDR=0x10008000'
-            DTB_TARGET='imx6q-tek3.dtb imx6dl-tek3.dtb imx6q-tep5.dtb imx6dl-tep5.dtb'
-            TARGET_DEVICE=tek3_6dq
-    fi
-
+	elif [[ "$CPU_MODULE" == "evk_8mq" ]]; then
+            UBOOT_CONFIG='mx8mq_evk_android_defconfig'
+            KERNEL_IMAGE='Image'
+            KERNEL_CONFIG='android_defconfig'
+            DTB_TARGET='fsl-imx8mq-evk.dtb'
+            TARGET_DEVICE=evk_8mq
+            TARGET_DEVICE_NAME=imx8mq
+	fi
 fi
-
 
 recipe() {
     local TMP_PWD="${PWD}"
@@ -93,24 +92,32 @@ heat() {
     local TMP_PWD="${PWD}"
     case "${PWD}" in
         "${TOP}")
-            cd "${TMP_PWD}"
+            # cd "${TMP_PWD}"
             cd ${PATH_UBOOT} && heat "$@" || return $?
             cd ${PATH_KERNEL} && heat "$@" || return $?
             cd "${TMP_PWD}"
-            source build/envsetup.sh
-            lunch "$TARGET_DEVICE"-eng
+            lunch "$TARGET_DEVICE"-user
             make "$@" || return $?
+#           make "$@" PRODUCT-"$TARGET_DEVICE"-user dist || return $?
             ;;
         "${PATH_KERNEL}"*)
-#            export CROSS_COMPILE="${TOP}/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
+            export CROSS_COMPILE="${TOP}/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
             cd "${PATH_KERNEL}"
-#            make "$@" $KERNEL_IMAGE LOADADDR=0x10008000 $KERNEL_CFLAGS || return $?
-            make "$@" $KERNEL_IMAGE LOADADDR=0x10008000 || return $?
-            make "$@" modules || return $?
-            make "$@" $DTB_TARGET || return $?
+			rm "${TOP}"/device/fsl/"${TARGET_DEVICE}"/wifi-firmware/QCA9377/wlan.ko
+			rm -rf ../modules/lib
+            make "$@" $KERNEL_CFLAGS || return $?
+            make "$@" || return $?
+            make "$@" modules_install INSTALL_MOD_PATH=../modules || return $?
+			cd drivers/net/wireless/qcacld-2.0
+            make "$@" clean || return $?
+            make "$@" || return $?
+            KERNEL_SRC=../../../../../kernel_imx make "$@" modules_install INSTALL_MOD_PATH=../modules || return $?
+            cd "${PATH_KERNEL}"
+			cp ../modules/lib/modules/4.9.78/extra/wlan.ko "${TOP}"/device/fsl/"${TARGET_DEVICE}"/wifi-firmware/QCA9377/
             ;;
         "${PATH_UBOOT}"*)
-#            export CROSS_COMPILE="${TOP}/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-"
+            #export CROSS_COMPILE="${TOP}/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+			export CROSS_COMPILE=/usr/bin/aarch64-linux-gnu-
             cd "${PATH_UBOOT}"
             make "$@" || return $?
             ;;
@@ -131,20 +138,18 @@ cook() {
         "${TOP}")
             cd ${PATH_UBOOT} && cook "$@" || return $?
             cd ${PATH_KERNEL} && cook "$@" || return $?
-
             cd "${TMP_PWD}"
-            source build/envsetup.sh
-            lunch "$TARGET_DEVICE"-eng
+            lunch "$TARGET_DEVICE"-userdebug
             make "$@" || return $?
             ;;
         "${PATH_KERNEL}"*)
-#            export CROSS_COMPILE="${TOP}/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
+            #export CROSS_COMPILE="${TOP}/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
             cd "${PATH_KERNEL}"
             make "$@" $KERNEL_CONFIG || return $?
             heat "$@" || return $?
             ;;
         "${PATH_UBOOT}"*)
-#            export CROSS_COMPILE="${TOP}/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-"
+            #export CROSS_COMPILE="${TOP}/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
             cd "${PATH_UBOOT}"
             make "$@" $UBOOT_CONFIG || return $?
             heat "$@" || return $?
@@ -169,10 +174,13 @@ throw() {
             ;;
         "${PATH_KERNEL}"*)
             cd "${PATH_KERNEL}"
+#           make "$@" $KERNEL_CONFIG || return $?
+#           make "$@" $KERNEL_CFLAGS || return $?
             make "$@" distclean || return $?
             ;;
         "${PATH_UBOOT}"*)
             cd "${PATH_UBOOT}"
+#           make "$@" $UBOOT_CONFIG || return $?
             make "$@" distclean || return $?
             ;;
         *)
@@ -186,78 +194,125 @@ throw() {
 
 flashcard() {
     local TMP_PWD="${PWD}"
-
+	PATH_OUT="${TOP}/out/target/product/${TARGET_DEVICE}"
     dev_node="$@"
     echo "$dev_node start"
-    cd "${TOP}"
-    sudo ./device/fsl/common/tools/fsl-sdcard-partition.sh ${dev_node}
-    sync
+	cd "${PATH_OUT}"
+	sudo $TOP/device/fsl/common/tools/tn-sdcard-partition.sh -f ${TARGET_DEVICE_NAME} -c 7 ${dev_node}
+	sync
+    echo "Flash Done!!!"
+    cd "${TMP_PWD}"
+}
+
+flashuboot() {
+    local TMP_PWD="${PWD}"
+	PATH_OUT="${TOP}/out/target/product/${TARGET_DEVICE}"
+    dev_node="$@"
     sudo hdparm -z ${dev_node}
     sync
+
+    echo == download the android boot ==
+    sudo dd if=${PATH_OUT}/u-boot-${TARGET_DEVICE_NAME}.imx of=${dev_node}1 bs=1M oflag=dsync
+    sleep 1
+
+    echo == download the android recovery ==
+    sudo dd if=${PATH_OUT}/u-boot-${TARGET_DEVICE_NAME}.imx of=${dev_node}2 bs=1M oflag=dsync
+    sleep 1
+
+	echo == Erase the environment variables ==
+	sudo dd if=/dev/zero of=${dev_node} bs=1k seek=1 count=1023 oflag=dsync
+	sleep 1
+	sudo dd if=/dev/zero of=${dev_node} bs=1M seek=1 count=3 oflag=dsync
+	sleep 1
+
+    echo == download the boot loader ==
+    sudo dd if=${PATH_OUT}/u-boot-${TARGET_DEVICE_NAME}.imx of=${dev_node} bs=1k seek=33 oflag=dsync
+    sleep 1
+
+	sync
+    echo "Flash Done!!!"
+
+}
+
+flashemmc() {
+    local TMP_PWD="${PWD}"
+	PATH_OUT="${TOP}/out/target/product/${TARGET_DEVICE}"
+    dev_node="$@"
+    echo "$dev_node start"
+	cd "${PATH_OUT}"
+	sudo $TOP/device/fsl/common/tools/fsl-sdcard-partition-source.sh -f ${TARGET_DEVICE} -c 7 ${dev_node}
+	sync
+    sleep 1
+	sudo $TOP/device/fsl/common/tools/gpt_partition_move -d ${dev_node} -s 8192
+    sync
+    sleep 1
+
+	echo == Erase the environment variables ==
+	sudo dd if=/dev/zero of=${dev_node} bs=1k seek=1 count=1023 oflag=dsync
+	sleep 1
+	sudo dd if=/dev/zero of=${dev_node} bs=1M seek=1 count=3 oflag=dsync
+	sleep 1
+
+    cd "${TOP}"
+    sudo hdparm -z ${dev_node}
+    sync
+
+    echo == download the boot loader ==
+    # sudo dd if=${PATH_OUT}/u-boot-${TARGET_DEVICE_NAME}_defconfig.imx of=${dev_node} bs=1k seek=33 oflag=dsync
+    sudo dd if=${PATH_OUT}/obj/BOOTLOADER_OBJ/SPL of=${dev_node} bs=1k seek=33 oflag=dsync
+    sleep 1
+
     sudo mkfs.vfat -F 32 ${dev_node}1 -n boot;sync
 
-    if [[ "$CPU_TYPE" == "imx7" ]]; then
-        sudo dd if=./out/target/product/$TARGET_DEVICE/boot.img of=${dev_node}1; sync
-        sleep 1
-    else
-        mkdir $IMX_PATH
-        sudo mount ${dev_node}1 $IMX_PATH;
-        sudo cp $PATH_UBOOT/u-boot.img $IMX_PATH/; sync
-        sudo cp $PATH_KERNEL/arch/arm/boot/zImage $IMX_PATH/zImage; sync
+	mkdir $IMX_PATH
+    sudo mount ${dev_node}1 $IMX_PATH;
+    sudo cp ${PATH_OUT}/obj/BOOTLOADER_OBJ/u-boot.img $IMX_PATH/; sync
+    sudo cp ${PATH_OUT}/obj/KERNEL_OBJ/arch/arm64/boot/Image $IMX_PATH/Image; sync
 
-        if [[ "$TARGET_DEVICE" == "edm1cf_6dq" || "$TARGET_DEVICE" == "edm1cf_pmic_6dq"  ]]; then
-            sudo cp $PATH_KERNEL/arch/arm/boot/dts/imx6q-edm1-cf_"$BASEBOARD".dtb $IMX_PATH/imx6q-edm1-cf_"$BASEBOARD".dtb; sync
-            sudo cp $PATH_KERNEL/arch/arm/boot/dts/imx6dl-edm1-cf_"$BASEBOARD".dtb $IMX_PATH/imx6dl-edm1-cf_"$BASEBOARD".dtb;sync
-            sudo cp $PATH_KERNEL/arch/arm/boot/dts/imx6q-edm1-cf-pmic_"$BASEBOARD".dtb $IMX_PATH/imx6q-edm1-cf-pmic_"$BASEBOARD".dtb; sync
-            sudo cp $PATH_KERNEL/arch/arm/boot/dts/imx6dl-edm1-cf-pmic_"$BASEBOARD".dtb $IMX_PATH/imx6dl-edm1-cf-pmic_"$BASEBOARD".dtb;sync
-            sudo cp $PATH_KERNEL/arch/arm/boot/dts/imx6qp-edm1-cf-pmic_"$BASEBOARD".dtb $IMX_PATH/imx6qp-edm1-cf-pmic_"$BASEBOARD".dtb; sync
-        elif [[ "$TARGET_DEVICE" == "pico_6dq" ]]; then
-            sudo cp $PATH_KERNEL/arch/arm/boot/dts/imx6q-pico_"$BASEBOARD".dtb $IMX_PATH/imx6q-pico_"$BASEBOARD".dtb; sync
-            sudo cp $PATH_KERNEL/arch/arm/boot/dts/imx6dl-pico_"$BASEBOARD".dtb $IMX_PATH/imx6dl-pico_"$BASEBOARD".dtb; sync
-        elif [[ "$TARGET_DEVICE" == "tek3_6dq" ]]; then
-            sudo cp $PATH_KERNEL/arch/arm/boot/dts/imx6q-tek3.dtb $IMX_PATH/imx6q-tek3.dtb; sync
-            sudo cp $PATH_KERNEL/arch/arm/boot/dts/imx6dl-tek3.dtb $IMX_PATH/imx6dl-tek3.dtb;sync
-            sudo cp $PATH_KERNEL/arch/arm/boot/dts/imx6q-tep5.dtb $IMX_PATH/imx6q-tep5.dtb; sync
-            sudo cp $PATH_KERNEL/arch/arm/boot/dts/imx6dl-tep5.dtb $IMX_PATH/imx6dl-tep5.dtb;sync
-       fi
+    # donwload the environment settings
+    echo == download the environment - Display: "$OUTPUT_DISPLAY" ==
+		sudo cp $TOP/out/target/product/$TARGET_DEVICE/obj/KERNEL_OBJ/*.dtb $IMX_PATH/.; sync
+        sudo cp ./device/fsl/"$TARGET_DEVICE"/uenv/chmodel.sh $IMX_PATH/chmodel.sh; sync
+        sudo cp ./device/fsl/"$TARGET_DEVICE"/uenv/uEnv.txt.*.* $IMX_PATH/.; sync
+		sudo cp ./device/fsl/"$TARGET_DEVICE"/uenv/uEnv.txt."$BASEBOARD"."$OUTPUT_DISPLAY" $IMX_PATH/uEnv.txt; sync
 
-        # donwload the environment settings
-        echo == download the environment - Display: "$OUTPUT_DISPLAY" ==
-        if [[ "$TARGET_DEVICE" == "pico_6dq" ]]; then
-             sudo cp ./device/fsl/"$TARGET_DEVICE"/uenv/uEnv.txt."$BASEBOARD"."$OUTPUT_DISPLAY" $IMX_PATH/uEnv.txt; sync
-        else
-            sudo cp ./device/fsl/"$TARGET_DEVICE"/uenv/uEnv.txt."$OUTPUT_DISPLAY" $IMX_PATH/uEnv.txt; sync
-        fi
-        # download the ramdisk
+    # download the ramdisk
+    if [[ "$CPU_TYPE" == "imx8" ]]; then
         echo == download the ramdisk ==
-        sudo mkimage -A arm -O linux -T ramdisk -C none -a 0x10800800 -n "Android Root Filesystem" -d ./out/target/product/$TARGET_DEVICE/ramdisk.img ./out/target/product/$TARGET_DEVICE/uramdisk.img
-        sudo cp ./out/target/product/$TARGET_DEVICE/uramdisk.img $IMX_PATH/;sync
+        sudo mkimage -A arm -O linux -T ramdisk -C none -a 0x43800000 -n "Android Root Filesystem" -d ${PATH_OUT}/ramdisk.img ${PATH_OUT}/uramdisk.img
+        sudo cp ${PATH_OUT}/uramdisk.img $IMX_PATH/;sync
+	elif [[ "$CPU_TYPE" == "imx7" ]]; then
+        echo == download the ramdisk ==
+        sudo mkimage -A arm -O linux -T ramdisk -C none -a 0x83800000 -n "Android Root Filesystem" -d ${PATH_OUT}/ramdisk.img ${PATH_OUT}/uramdisk.img
+        sudo cp ${PATH_OUT}/uramdisk.img $IMX_PATH/;sync
+	elif [[ "$CPU_TYPE" == "imx6" ]]; then
+        echo == download the ramdisk ==
+        sudo mkimage -A arm -O linux -T ramdisk -C none -a 0x10800800 -n "Android Root Filesystem" -d ${PATH_OUT}/ramdisk.img ${PATH_OUT}/uramdisk.img
+        sudo cp ${PATH_OUT}/uramdisk.img $IMX_PATH/;sync
     fi
-    # download the android system
-    echo == download the system ==
-    sudo dd if=./out/target/product/$TARGET_DEVICE/system_raw.img of=${dev_node}5;sync
+
+    echo == download the android recovery ==
+    sudo dd if=${PATH_OUT}/boot.img of=${dev_node}2 bs=1M oflag=dsync
     sleep 1
-    echo == download the recovery ==
-    sudo dd if=./out/target/product/$TARGET_DEVICE/recovery.img of=${dev_node}2; sync
+
+    echo == download the android system ==
+    sudo dd if=${PATH_OUT}/system_raw.img of=${dev_node}3 bs=1M oflag=dsync
+    sleep 1
+
+    echo == download the vendor ==
+    sudo dd if=${PATH_OUT}/vendor_raw.img of=${dev_node}8 bs=1M oflag=dsync
+    sleep 1
+
+    echo == download the vbmeta ==
+    sudo dd if=${PATH_OUT}/vbmeta-${TARGET_DEVICE_NAME}.dtb.img of=${dev_node}12 bs=1M oflag=dsync
     sleep 1
 
     sudo umount ${dev_node}*
     sudo rm -rf $IMX_PATH
+	sudo rm ${PATH_OUT}/uramdisk.img
     sync
     sleep 1
-
-    if [[ "$CPU_TYPE" == "imx7" ]]; then
-        sudo dd if=$PATH_UBOOT/u-boot.imx of="$@" bs=512 seek=2; sync
-        echo == flash the u-boot.imx finish ==
-        sleep 1
-    else
-        sudo dd if=$PATH_UBOOT/SPL of="$@" bs=1k seek=1; sync
-        echo == flash the spl finish ==
-        sleep 1
-    fi
-
     echo "Flash Done!!!"
-
     cd "${TMP_PWD}"
 }
 

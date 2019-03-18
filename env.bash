aa4497e03c8505144b896732a8a9a1bb4c8846e9
Wig Cheng <wig.cheng@technexion.com>
2019-03-18 10:32:20 +0800

    pico-imx8m: remove redundant functions from env.bash

--- env.bash
+++ env.bash
@@ -22,7 +22,7 @@ export MY_ANDROID=$TOP
 export LC_ALL=C
 export DRAM_SIZE_1G=false
 
-# TARGET support: wandboard,edm1cf,picosom,edm1cf_6sx
+# TARGET support: pico-imx8m, pico-imx8mm
 IMX_PATH="./mnt"
 SYS_PATH="./tmp"
 MODULE=$(basename $BASH_SOURCE)
@@ -85,17 +85,9 @@ heat() {
         "${PATH_KERNEL}"*)
             export CROSS_COMPILE="${TOP}/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
             cd "${PATH_KERNEL}"
-            rm "${TOP}"/device/fsl/"${TARGET_DEVICE}"/wifi-firmware/wlan.ko
             rm -rf ../modules/lib
             make "$@" $KERNEL_CFLAGS || return $?
-            #make "$@" || return $?
             make "$@" modules_install INSTALL_MOD_PATH=../modules || return $?
-            #cd drivers/net/wireless/qcacld-2.0
-            #make "$@" clean || return $?
-            #make "$@" || return $?
-            #KERNEL_SRC=../../../../../kernel_imx make "$@" modules_install INSTALL_MOD_PATH=../modules || return $?
-            #cd "${PATH_KERNEL}"
-            #cp ../modules/lib/modules/4.9.78*/extra/wlan.ko "${TOP}"/device/fsl/"${TARGET_DEVICE}"/wifi-firmware/
             ;;
         "${PATH_UBOOT}"*)
             export CROSS_COMPILE="${TOP}/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-gnu/bin/aarch64-linux-gnu-"
@@ -188,118 +180,6 @@ flashcard() {
   sync
   echo "Flash Done!!!"
   cd "${TMP_PWD}"
-}
-
-flashuboot() {
-    local TMP_PWD="${PWD}"
-	PATH_OUT="${TOP}/out/target/product/${TARGET_DEVICE}"
-    dev_node="$@"
-    sudo hdparm -z ${dev_node}
-    sync
-
-    echo == download the android boot ==
-    sudo dd if=${PATH_OUT}/u-boot-${TARGET_DEVICE_NAME}.imx of=${dev_node}1 bs=1M oflag=dsync
-    sleep 1
-
-    echo == download the android recovery ==
-    sudo dd if=${PATH_OUT}/u-boot-${TARGET_DEVICE_NAME}.imx of=${dev_node}2 bs=1M oflag=dsync
-    sleep 1
-
-	echo == Erase the environment variables ==
-	sudo dd if=/dev/zero of=${dev_node} bs=1k seek=1 count=1023 oflag=dsync
-	sleep 1
-	sudo dd if=/dev/zero of=${dev_node} bs=1M seek=1 count=3 oflag=dsync
-	sleep 1
-
-    echo == download the boot loader ==
-    sudo dd if=${PATH_OUT}/u-boot-${TARGET_DEVICE_NAME}.imx of=${dev_node} bs=1k seek=33 oflag=dsync
-    sleep 1
-
-	sync
-    echo "Flash Done!!!"
-
-}
-
-flashemmc() {
-    local TMP_PWD="${PWD}"
-	PATH_OUT="${TOP}/out/target/product/${TARGET_DEVICE}"
-    dev_node="$@"
-    echo "$dev_node start"
-	cd "${PATH_OUT}"
-	sudo $TOP/device/fsl/common/tools/fsl-sdcard-partition-source.sh -f ${TARGET_DEVICE} -c 7 ${dev_node}
-	sync
-    sleep 1
-	sudo $TOP/device/fsl/common/tools/gpt_partition_move -d ${dev_node} -s 8192
-    sync
-    sleep 1
-
-	echo == Erase the environment variables ==
-	sudo dd if=/dev/zero of=${dev_node} bs=1k seek=1 count=1023 oflag=dsync
-	sleep 1
-	sudo dd if=/dev/zero of=${dev_node} bs=1M seek=1 count=3 oflag=dsync
-	sleep 1
-
-    cd "${TOP}"
-    sudo hdparm -z ${dev_node}
-    sync
-
-    echo == download the boot loader ==
-    # sudo dd if=${PATH_OUT}/u-boot-${TARGET_DEVICE_NAME}_defconfig.imx of=${dev_node} bs=1k seek=33 oflag=dsync
-    sudo dd if=${PATH_OUT}/obj/BOOTLOADER_OBJ/SPL of=${dev_node} bs=1k seek=33 oflag=dsync
-    sleep 1
-
-    sudo mkfs.vfat -F 32 ${dev_node}1 -n boot;sync
-
-	mkdir $IMX_PATH
-    sudo mount ${dev_node}1 $IMX_PATH;
-    sudo cp ${PATH_OUT}/obj/BOOTLOADER_OBJ/u-boot.img $IMX_PATH/; sync
-    sudo cp ${PATH_OUT}/obj/KERNEL_OBJ/arch/arm64/boot/Image $IMX_PATH/Image; sync
-
-    # donwload the environment settings
-    echo == download the environment - Display: "$OUTPUT_DISPLAY" ==
-		sudo cp $TOP/out/target/product/$TARGET_DEVICE/obj/KERNEL_OBJ/*.dtb $IMX_PATH/.; sync
-        sudo cp ./device/fsl/"$TARGET_DEVICE"/uenv/chmodel.sh $IMX_PATH/chmodel.sh; sync
-        sudo cp ./device/fsl/"$TARGET_DEVICE"/uenv/uEnv.txt.*.* $IMX_PATH/.; sync
-		sudo cp ./device/fsl/"$TARGET_DEVICE"/uenv/uEnv.txt."$BASEBOARD"."$OUTPUT_DISPLAY" $IMX_PATH/uEnv.txt; sync
-
-    # download the ramdisk
-    if [[ "$CPU_TYPE" == "imx8" ]]; then
-        echo == download the ramdisk ==
-        sudo mkimage -A arm -O linux -T ramdisk -C none -a 0x43800000 -n "Android Root Filesystem" -d ${PATH_OUT}/ramdisk.img ${PATH_OUT}/uramdisk.img
-        sudo cp ${PATH_OUT}/uramdisk.img $IMX_PATH/;sync
-	elif [[ "$CPU_TYPE" == "imx7" ]]; then
-        echo == download the ramdisk ==
-        sudo mkimage -A arm -O linux -T ramdisk -C none -a 0x83800000 -n "Android Root Filesystem" -d ${PATH_OUT}/ramdisk.img ${PATH_OUT}/uramdisk.img
-        sudo cp ${PATH_OUT}/uramdisk.img $IMX_PATH/;sync
-	elif [[ "$CPU_TYPE" == "imx6" ]]; then
-        echo == download the ramdisk ==
-        sudo mkimage -A arm -O linux -T ramdisk -C none -a 0x10800800 -n "Android Root Filesystem" -d ${PATH_OUT}/ramdisk.img ${PATH_OUT}/uramdisk.img
-        sudo cp ${PATH_OUT}/uramdisk.img $IMX_PATH/;sync
-    fi
-
-    echo == download the android recovery ==
-    sudo dd if=${PATH_OUT}/boot.img of=${dev_node}2 bs=1M oflag=dsync
-    sleep 1
-
-    echo == download the android system ==
-    sudo dd if=${PATH_OUT}/system_raw.img of=${dev_node}3 bs=1M oflag=dsync
-    sleep 1
-
-    echo == download the vendor ==
-    sudo dd if=${PATH_OUT}/vendor_raw.img of=${dev_node}8 bs=1M oflag=dsync
-    sleep 1
-
-    echo == download the vbmeta ==
-    sudo dd if=${PATH_OUT}/vbmeta-${TARGET_DEVICE_NAME}.dtb.img of=${dev_node}12 bs=1M oflag=dsync
-    sleep 1
-
-    sudo umount ${dev_node}*
-    sudo rm -rf $IMX_PATH
-	sudo rm ${PATH_OUT}/uramdisk.img
-    sync
-    sleep 1
-    echo "Flash Done!!!"
-    cd "${TMP_PWD}"
 }
 
 merge_restricted_extras() {

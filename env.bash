3c5051827154e51f91c340141a08d7548885101f
Tifa Chen <tifa.chen@technexion.com>
2025-10-08 19:55:24 -0700

    env.bash: Workaround: update IW612 fw
    Update IW612 fw to IW612-18.99.3.p26.7 version.
    For fix fw crash when wifi enable, and can't reload fw again.
    
    error log:
      F         : FW trigger fw dump
      F WIFI auto_fw_reload: fw_reload=1
      W         : Fail to poll firmware status: firmwarestat=0x0
      W         : FW failed to be active in time!
      W         : wlan_dnld_fw fail ret=0xffffffff
      E         : Firmware Init Failed

--- env.bash
+++ env.bash
@@ -309,6 +309,12 @@ merge_restricted_extras() {
 	unset _kernel_ver _kernel_tool
 
 	unset _imx_android_ver _toolchain_ver _arm_toolchain _dest
+
+	# WA: IW612 fw
+	local _iw612_fw="sduart_nw61x_v1.bin.se"
+	wget -c -t 0 --timeout=60 --waitretry=60 https://download.technexion.com/development_resources/NXP/android/${_android_ver}/imx-firmware/${_iw612_fw}
+	mv ${_iw612_fw} ${TOP}/vendor/nxp/imx-firmware/nxp/FwImage_IW612_SD/${_iw612_fw}
+	unset _iw612_fw
 }
 
 get_tn_firmware() {
